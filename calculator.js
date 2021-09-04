9e918879ea3e36b170de606196791378990b2cb2
EventBridge <vincentwhitetran@gmail.com>
2021-09-04 15:10:12 +1000

    keypad input and clear

--- calculator.js
+++ calculator.js
@@ -1,3 +1,20 @@
+let displayNumber = document.querySelector('.screen');
+let variableDisplay = displayNumber;
+
+const keypad = document.querySelectorAll('.keypad');
+
+keypad.forEach((item) => {
+    item.addEventListener('click', () => {
+        displayNumber.textContent = displayNumber.textContent + item.textContent;
+    });
+});
+
+const clearButton = document.querySelector('.clear');
+
+clearButton.addEventListener('click', () => {
+    displayNumber.textContent = '';
+});
+
 function add(a,b) {
     return a+b;
 }
